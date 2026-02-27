8e15ff528bf587425c98a85a5958fbb19ad2d4fa
Gonzalo <gonzalogomezruiz14@gmail.com>
2026-02-27 14:30:13 +0100

    nuevas clases

--- clases/clase_itemInventario.py
+++ clases/clase_itemInventario.py
@@ -1,6 +1,14 @@
 #Clase base
 class ItemInventario:
-    def __init__(self,nombre, id, nombre, proveedor):
+    def __init__(self,nombre, id, proveedor):
         self.nombre = nombre
         self.id = id
         self.proveedor = proveedor
+
+    def __str__(self):
+        return f"{self.nombre} , {self.id}, {self.proveedor}"
+
+
+
+medicamentos = ItemInventario('paracetamol',123,'pyzher' )
+print(medicamentos)

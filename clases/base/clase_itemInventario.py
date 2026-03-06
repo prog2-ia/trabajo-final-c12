826fe322cf91773c9304b5c9a6acff05bece13c0
gonzalo <gonzalogomezruiz14@gmail.com>
2026-03-06 14:35:20 +0100

    cambios

--- clases/base/clase_itemInventario.py
+++ clases/base/clase_itemInventario.py
@@ -1,12 +1,31 @@
 #Clase base
 class ItemInventario:
-    def __init__(self,nombre, id, proveedor):
+
+    categoria = "Item de laboratorio"
+
+    def __init__(self,nombre, id_item, proveedor, cantidad):
         self.nombre = nombre
-        self.id = id
+        self.id_item = id_item
         self.proveedor = proveedor
+        self.cantidad = cantidad
 
     def __str__(self):
-        return f"{self.nombre} , {self.id}, {self.proveedor}"
+        return f"{self.nombre} ,id : {self.id}, proveedor : {self.proveedor}, cantidad : {self.cantidad}"
+
+
+    def consumir(self, unidades):
+        self.cantidad += unidades
+        if unidades > self.cantidad:
+            print(f'Se han añadido {unidades} unidades')
+        else:
+            print(f'se han acabado {unidades} unidades')
+
+
+    def mostrar_info(self):
+        print(f'nombre : {self.nombre}')
+        print(f'id_item : {self.id_item}')
+        print(f'proveedor : {self.proveedor}')
+        print(f'cantidad : {self.cantidad}')
 
 
 

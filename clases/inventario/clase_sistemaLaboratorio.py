0893786629b813d7b64e510a15b030d556242e83
alvaro-imanol-castillo-romero <imanolromero201@gmail.com>
2026-03-13 14:18:07 +0100

    Clase sistema Laboratorio actualizada

--- clases/inventario/clase_sistemaLaboratorio.py
+++ clases/inventario/clase_sistemaLaboratorio.py
@@ -1,2 +1,39 @@
-class Laboratorio:
-    pass
+from clases.inventario.clase_gestorInventario import GestorInventario
+
+
+class SisteLaboratorio:
+    def __init__(self):
+        self.gestor = None
+
+        def inicializar(self):
+            print("--- Iniciando el Sistema ---")
+            self.gestor = GestorInventario()
+            print("Sistema listo.")
+
+        def mostrarMenu(self):
+            print("\n" + "=" * 30)
+            print("   SISTEMA DE LABORATORIO")
+            print("=" * 30)
+            print("1. Registrar nuevo ítem (Reactivo/Equipo)")
+            print("2. Ver inventario completo")
+            print("3. Buscar ítem por nombre")
+            print("4. Eliminar ítem")
+            print("5. Salir")
+            print("=" * 30)
+
+        def ejecutar(self):
+            self.inicializar()
+
+            while True:
+                self.mostrarMenu()
+                opcion = input("Seleccione una opción: ")
+
+                if opcion == "1":
+                    print("Llamando a gestor.agregar_item()...")
+                elif opcion == "2":
+                    print("Mostrando inventario...")
+                elif opcion == "5":
+                    print("Cerrando sistema. ¡Adiós!")
+                    break
+                else:
+                    print("Opción no válida, intente de nuevo.")

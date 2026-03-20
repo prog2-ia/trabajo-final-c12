1e8170b7333d6ffeb02b0fb4191d872f099a7c78
alvaro-imanol-castillo-romero <imanolromero201@gmail.com>
2026-03-20 14:55:25 +0100

    Cambios en las clases ItemInventario y Consumible

--- clases/base/clase_itemInventario.py
+++ clases/base/clase_itemInventario.py
@@ -1,41 +1,14 @@
-# Clase base
+
 class ItemInventario:
 
-    categoria = "Item de laboratorio"
-
-    def __init__(self, nombre, id_item, proveedor, cantidad):
-        self.nombre = nombre
+    def __init__(self,id_item,nombre,cantidad,unidad_medida, requisitos_seguridad, umbral_critico):
         self.id_item = id_item
-        self.proveedor = proveedor
-        self._cantidad = 0
+        self.nombre = nombre
         self.cantidad = cantidad
+        self.unidad_medida = unidad_medida
+        self.requisitos_seguridad = requisitos_seguridad
+        self.umbral_critico = umbral_critico
 
     def __str__(self):
-        return f"{self.nombre}, id: {self.id_item}, proveedor: {self.proveedor}, cantidad: {self.cantidad}"
+        return f"[{self.id_item}]{self.nombre} - {self.cantidad} {self.unidad_medida}"
 
-    def consumir(self, unidades):
-        if unidades <= 0:
-            print("Las unidades a consumir deben ser positivas")
-        elif unidades > self.cantidad:
-            print("No hay suficientes unidades")
-        else:
-            self._cantidad -= unidades
-            print(f"Se han consumido {unidades} unidades")
-
-    def mostrar_info(self):
-        print(f"nombre: {self.nombre}")
-        print(f"id_item: {self.id_item}")
-        print(f"proveedor: {self.proveedor}")
-        print(f"cantidad: {self.cantidad}")
-
-    @property
-    def cantidad(self):
-        return self._cantidad
-
-    @cantidad.setter
-    def cantidad(self, valor):
-        if valor < 0:
-            print("La cantidad no puede ser negativa")
-        else:
-            self._cantidad += valor
-            print(f"Se han añadido {valor} unidades")
--- clases/consumibles/clase_consumible.py
+++ clases/consumibles/clase_consumible.py
@@ -1,3 +1,14 @@
-from main import ItemInventario
-class consumible:
-    def __init__(self, cantidad):
+from clases.base.clase_itemInventario import ItemInventario
+
+class Consumible(ItemInventario):
+    def __init__(self,id_item,nombre,cantidad,unidad_medida, requisitos_seguridad, umbral_critico,fecha_caducidad,lote):
+        super.__init__(id_item,nombre,cantidad,unidad_medida, requisitos_seguridad, umbral_critico)
+        self.fecha_caducidad = fecha_caducidad
+        self.lote = lote
+
+    def __str__(self):
+        info_item=super().__str__()
+        return f"{info_item} | lote: {self.lote} | caduca: {self.fecha_caducidad}"
+
+
+

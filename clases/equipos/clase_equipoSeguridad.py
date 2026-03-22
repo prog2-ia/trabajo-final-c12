c8eb917c9046aee8a203023d3ca6dff3788196be
imanolromero201-design <imanolromero201@gmail.com>
2026-03-22 18:42:31 +0100

    actualizacion equipoSeguridad

--- clases/equipos/clase_equipoSeguridad.py
+++ clases/equipos/clase_equipoSeguridad.py
@@ -1,2 +1,15 @@
-class consumible:
-    pass
+from clases.equipos.clase_equipamiento import Equipamiento
+
+class EquipoSeguridad(Equipamiento):
+    def __init__ (self,id_item,nombre,cantidad, requisitos_seguridad, umbral_critico,fecha_manteniemiento,ubicacion , certificado,estado="Operativo"):
+        super().__init__(nombre,nombre, cantidad, requisitos_seguridad, umbral_critico, fecha_manteniemiento, estado)
+        self.ubicacion = ubicacion
+        self.certificado = certificado
+
+    def __str__(self):
+        info_equipo=super().__str__()
+        return f"{info_equipo} | Ubicacion: {self.ubicacion} | Certificado: {self.certificado}"
+
+    def realizar_prueba(self):
+        print(f"Simulacro realizado con el equipo {self.nombre} en la ubicacion {self.ubicacion}")
+

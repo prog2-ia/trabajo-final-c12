88d556ff612698ce43690e16468af2a6c4746fc8
imanolromero201-design <imanolromero201@gmail.com>
2026-03-27 13:12:12 +0100

    Actualización Sistema_laboratorio (menú)

--- clases/inventario/auditoria.py
+++ clases/inventario/auditoria.py
@@ -2,7 +2,15 @@
 """Definimos la clase Auditorio, que lo unico que hace es tener 2 procesos, registrar el uso y ver el historial de uso
 estos datos se guardan en una lista "Historial de uso" """
 class Auditoria:
+    def __init__(self):
+        if not hasattr(self, 'historial_uso'):
+            self.historial_uso = []
+
+
     def registrar_uso(self,usuario,cantidad,motivo):
+        if not hasattr(self, 'historial_uso'):
+            self.historial_uso = []
+
         registro= f"Usuario {usuario} | Retiró {cantidad} | Motivo {motivo}"
         self.historial_uso.append(registro)
         print(f"Registro Guardado: {registro}")
@@ -14,6 +22,6 @@ class Auditoria:
             print("No existe historial de uso")
 
         else:
-            for registro in self.historial_uso:
+            for i,registro in enumerate( self.historial_uso,1):
                 print(f"-{registro}")
 
--- clases/inventario/clase_sistemaLaboratorio.py
+++ clases/inventario/clase_sistemaLaboratorio.py
@@ -21,13 +21,13 @@ class SistemaLaboratorio:
                 print(
                     " 1: Mostrar Inventario\n 2: Añadir Item\n 3: Borrar Item\n 4: Registrar uso de un Item\n 5: Salir del Sistema")
 
-                opcion = int(input("Introduce tu acción elegida: "))
+                opcion = input("Introduce tu acción elegida: ")
 
-                if opcion not in [1, 2, 3, 4, 5]:
-                    print("Introduce una opción válida")
+                if opcion not in ["1","2","3","4","5"]:
+                    print("\n","Introduce una opción válida")
                     continue
 
-                elif opcion == 1:
+                elif opcion == "1":
                     print(">> Inventario actual: ")
                     self.gestorInventario.mostrar_inventario()
 
@@ -42,39 +42,53 @@ class SistemaLaboratorio:
                         else:
                             print("Acción inválida")
 
-                elif opcion == 2:
+                elif opcion == "2":
                     print("¿Qué tipo de Item desea añadir?")
                     print("1: Consumible | 2: Equipo")
                     while True:
-                        tipo_item = int(input("Introduce tu elección: "))
-                        if tipo_item in [1, 2]:
+                        tipo_item = input("Introduce tu elección: ")
+                        if tipo_item in ["1","2"]:
                             break
                         else:
                             print("Introduce un tipo de Item correcto")
 
-                    if tipo_item == 1:
+                    if tipo_item == "1":
                         print("¿Que tipo de consumible quieres registrar?\n 1: Reactivo Químico | 2: Material Biológico")
                         while True:
-                            tipo_consumible = int(input("Introduce tu elección: "))
-                            if tipo_consumible in [1, 2]:
+                            tipo_consumible = input("Introduce tu elección: ")
+                            if tipo_consumible in ["1","2"]:
                                 break
                             else:
                                 print("Introduce un tipo de consumible correcto")
 
 
-                        # Datos comunes
+
                         id_item = input("Id Item: ")
                         nombre = input("Nombre: ")
-                        cantidad = int(input("Cantidad: "))
+                        cantidad = input("Cantidad: ")
+                        while True:
+                            if cantidad.isdigit():
+                                break
+
+                            else:
+                                print(f"{cantidad} no es un número, porfavor inserta un número entero ")
+                                cantidad = input("Cantidad: ")
+
                         unidad_medida = input("Unidad de Medida: ")
                         requisitos_seguridad = input("Requisitos de Seguridad: ")
-                        umbral_critico = int(input("Umbral Crítico: "))
+                        umbral_critico = input("Umbral Crítico: ")
+                        while True:
+                            if umbral_critico.isdigit():
+                                break
+                            else:
+                                print(f"{umbral_critico} valor no aceptado, porfavor inserta un número")
+                                umbral_critico=input("Umbral Critico: ")
+
                         fecha_caducidad = datetime.strptime(input("Fecha de caducidad (DD/MM/AAAA): "), "%d/%m/%Y")
                         lote = input("Lote: ")
 
 
-
-                        if tipo_consumible == 1:
+                        if tipo_consumible == "1":
                             formula_quimica = input("Introduce fórmula Química: ")
 
                             while True:
@@ -96,8 +110,11 @@ class SistemaLaboratorio:
                             self.gestorInventario.agregar_item(nuevo_item)
                             print("Item añadido con éxito.")
 
-                elif opcion == 5:
+                elif opcion == "5":
                     ejecutando_sistema = False
+
+                else:
+                    print("Introduce una opción válida")
 
         menu()
 
